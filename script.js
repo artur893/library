c294863f2efef27a05d09f4646a2b2806e18f42e
artur893 <arthal893@gmail.com>
2022-07-13 14:30:04 +0200

    New functions, add mess to code...

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//CREATING LIBRARY
+
 let myLibrary = []
 
 function Book(title, author, pages, readed) {
@@ -13,59 +15,81 @@ function addToLibrary(title, author, pages, readed) {
     myLibrary.push(book)
 }
 
+//DISPLAY BOOKS FROM CODE
 
-
-
-addToLibrary("Hobbit", "Tolkien", 372, true)
-addToLibrary("Inferno", "Dan Brown", 688, true)
-addToLibrary("Omerta", "Mario Puzo", 384, false)
-
-
-
-
-
-console.log(myLibrary)
-
-
-
+addToLibrary("Hobbit", "Tolkien", 356, true)
+displayBook()
 
 function displayBook() {
-    for (i = 0; i < myLibrary.length; i++) {
+    for (i = myLibrary.length - 1; i < myLibrary.length; i++) {
         const main = document.querySelector("main")
         const div = document.createElement("div")
-        const pTitle = document.createElement("p")
-        const pAuthor = document.createElement("p")
-        const pPages = document.createElement("p")
-        const pReaded = document.createElement("p")
+        const div2 = document.createElement("div")
+        const pTitle = document.createElement("div")
+        const pAuthor = document.createElement("div")
+        const pPages = document.createElement("div")
+        const pReaded = document.createElement("div")
         main.appendChild(div)
         pTitle.classList.add("title")
         pTitle.textContent = myLibrary[i].title
         div.appendChild(pTitle)
+        pTitle.appendChild(div2)
+        div2.classList.add("close")
+        div2.textContent = "X"
         pAuthor.textContent = myLibrary[i].author
         div.appendChild(pAuthor)
         pPages.textContent = myLibrary[i].pages + " pages"
         div.appendChild(pPages)
-        pReaded.textContent = myLibrary[i].readed
+        if (myLibrary[i].readed === true) {
+            pReaded.textContent = "Read"
+            pReaded.classList.add("readed")
+        }
+        if (myLibrary[i].readed === false) {
+            pReaded.textContent = "Not read"
+            pReaded.classList.add("notreaded")
+        }
+        div.classList.add("card")
         div.appendChild(pReaded)
     }
 }
 
-displayBook()
-
-
-//MODAL//
+//POPUP DISPLAY
 
 const closePopup = document.querySelector(".x")
 const openAdd = document.querySelector(".add")
 const popup = document.querySelector(".popup")
 const overlay = document.querySelector("#overlay")
 
-openAdd.addEventListener("click", function(){
+openAdd.addEventListener("click", function () {
     popup.classList.add("active")
     overlay.classList.add("active")
 })
 
-closePopup.addEventListener("click", function(){
+closePopup.addEventListener("click", function () {
     popup.classList.remove("active")
     overlay.classList.remove("active")
 })
+
+//POPUP LOGIC ADDING BOOK TO LIBRARY
+
+const title = document.getElementById("title")
+const author = document.getElementById("author")
+const pages = document.getElementById("pages")
+const readed = document.getElementById("readed")
+const submit = document.getElementById("submit")
+
+submit.addEventListener("click", function () {
+    addToLibrary(title.value, author.value, pages.value, readed.checked)
+    displayBook()
+    popup.classList.remove("active")
+    overlay.classList.remove("active")
+    title.value = ""
+    author.value = ""
+    pages.value = "0"
+    readed.checked = 0
+
+})
+
+//REMOVING BOOK FROM LIBRARY
+
+const close = document.querySelectorAll(".close")
